7f56dbab2cd139825b79421921d5dd530b4dbbb4
matheus-rn <matheus.1nascimento58@gmail.com>
2020-09-06 21:26:05 -0300

    add hook for graph

---
+++ pakedex-trab1/src/hooks/index.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { GraphProvider } from './graph';
+
+const AppProvider: React.FC = ({ children }) => (
+  <GraphProvider>
+    {children}
+  </GraphProvider>
+);
+
+export default AppProvider;
